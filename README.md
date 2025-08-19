6a2162088c3c738cb714c616bc5750d311275070
ydkulks <ydkulks2@gmail.com>
2025-08-19 16:47:20 +0530

    Initial Commit
    Apps to be installed on a fresh Arch Linux installation

---
+++ README.md
@@ -0,0 +1,3 @@
+# Arch BTW
+
+Arch config for programming
---
+++ apps.sh
@@ -0,0 +1,37 @@
+# Update
+sudo pacman -Syu
+
+# Install yay
+sudo pacman -S --needed git base-devel && git clone https://aur.archlinux.org/yay.git && cd yay && makepkg -si
+
+# Shell Tools
+yay -S zsh
+chsh -s $(which zsh)
+
+yay -S fzf ripgrep tmux man-db
+git clone https://github.com/tmux-plugins/tpm ~/.local/share/tmux/plugins/tpm
+
+curl -sS https://starship.rs/install.sh | sh
+
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.3/install.sh | bash
+
+curl https://sh.rustup.rs -sSf | sh
+
+# TUI
+yay -S ly
+sudo systemctl enable ly.service
+
+yay -S fastfetch btop htop yazi lazygit
+yay -S neovim
+
+# GUI
+yay -S kitty ghostty hyprland hyprlock-git waybar wofi pavucontrol pipewire-pulse swaync libnotify
+
+# GPU (AMD Radeon RX 6700 XT)
+# yay -S mesa libva-mesa-driver vulkan-radeon
+
+# Fonts
+yay -S ttf-jetbrains-mono-nerd ttf-roboto ttf-kannada-font noto-fonts-cjk
+
+# Apps
+yay -S dolphin zen-browser-bin key-pass
